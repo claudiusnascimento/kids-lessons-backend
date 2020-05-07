00fc4e05e13a69ef59f4a79c36da4b1a862ca6ff
Claudius Nascimento <cnascimento@uplexis.com.br>
2020-05-06 22:42:47 -0300

    add args and buildpack puppetweer

--- app/Services/DictionaryCrawler.js
+++ app/Services/DictionaryCrawler.js
@@ -6,7 +6,7 @@ class DictionaryCrawler {
 
 	async wordExists(word) {
 
-		const browser = await puppeteer.launch({headless: true});
+		const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]});
 		const page = await browser.newPage();
 		await page.goto(
 			'https://www.dicio.com.br/' + word,
